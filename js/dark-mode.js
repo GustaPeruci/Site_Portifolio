f26901b8c95811f8371fa04ce5ce7f37df2a9318
Gustavo Assis <gustavosss06@gmai.com>
2023-08-08 19:26:51 -0300

    Implementação de slides nos projetos e correções
    - Implementada na página "Projetos" para serem apresentados, passando como slides
    - Corrigido um erro no botão de dark mode que não identificava a situação ao mudar de página

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -4,16 +4,20 @@ function toggleDarkMode() {
     body.classList.toggle('dark-mode');
   }
   
-  // Verifica se o dark mode está ativado no início (por exemplo, se o usuário já o ativou em uma visita anterior)
-  if (localStorage.getItem('darkModeEnabled')) {
+  // Verifica o dark mode está ativado no início
+  const isDarkModeEnabled = localStorage.getItem('darkModeEnabled') === 'true';
+  if (isDarkModeEnabled) {
     document.body.classList.add('dark-mode');
   }
+  
+  // Atualiza o estado do checkbox com base no dark mode atual
+  document.getElementById('dark-mode-input').checked = isDarkModeEnabled;
   
   // Escuta o evento de clique no botão de alternância do dark mode
   document.getElementById('dark-mode-input').addEventListener('click', () => {
     const isDarkModeEnabled = document.body.classList.contains('dark-mode');
     if (isDarkModeEnabled) {
-      localStorage.removeItem('darkModeEnabled');
+      localStorage.setItem('darkModeEnabled', 'false');
     } else {
       localStorage.setItem('darkModeEnabled', 'true');
     }
